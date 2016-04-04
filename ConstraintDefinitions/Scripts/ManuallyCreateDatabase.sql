b08a9ab78c7a72e3bd9960bafa819e3e91aaf45d
dtndev <ianjdixon@yahoo.co.uk>
2016-04-05 00:44:38 +0100

    Add inital version of solution

---
+++ ConstraintDefinitions/Scripts/ManuallyCreateDatabase.sql
@@ -0,0 +1,20 @@
+﻿CREATE DATABASE [ConstraintDefinitions]
+GO
+
+USE [ConstraintDefinitions]
+GO
+
+CREATE TABLE [dbo].[Table1]
+(
+	[datetimeColumn] DATETIME NOT NULL, 
+    [intColumn] INT NOT NULL, 
+)
+GO
+
+ALTER TABLE [dbo].[Table1]
+ADD  CONSTRAINT [DF_Table1_Column1]  DEFAULT (CURRENT_TIMESTAMP) FOR [datetimeColumn]
+GO
+
+ALTER TABLE [dbo].[Table1]
+ADD CONSTRAINT [CK_Table1_intColumn] CHECK ([intColumn] In (1,3,7))
+GO
---
+++ ConstraintDefinitions/Tables/Table1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[datetimeColumn] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    [intColumn] INT NOT NULL, 
+    CONSTRAINT [CK_Table1_Column] CHECK ([intColumn] In (1,3,7))
+)
+GO
